33a1bd455a90d6efee126ace7c2e9d403bff5265
cnfd359 <tiova67@mail.ru>
2022-04-21 23:34:32 +0300

    Add main

---
+++ main.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+class LogLoss():
+    def __init__(self):
+        pass
+
+
+    def train(self, X, y):
+        self.X_train = X
+        self.y_train = y
+        w = np.random.normal(size=(X.shape[1],1))
+
+        while True:
+            
